e4d3d6d2f56595e2456d1c50c3355c5ca8097550
FelixAlvarado <felixalvarado653@gmail.com>
2021-01-07 14:27:15 -0800

    did check all 1s problem

---
+++ 12/16/2020/checkall1s.py
@@ -0,0 +1,45 @@
+# Given an array nums of 0s and 1s and an integer k, return True if all 1's are at least k places away from each other, otherwise return False.
+
+ 
+
+# Example 1:
+
+
+
+# Input: nums = [1,0,0,0,1,0,0,1], k = 2
+# Output: true
+# Explanation: Each of the 1s are at least 2 places away from each other.
+# Example 2:
+
+
+
+# Input: nums = [1,0,0,1,0,1], k = 2
+# Output: false
+# Explanation: The second 1 and third 1 are only one apart from each other.
+# Example 3:
+
+# Input: nums = [1,1,1,1,1], k = 0
+# Output: true
+# Example 4:
+
+# Input: nums = [0,1,0,1], k = 1
+# Output: true
+
+
+class Solution:
+    def kLengthApart(self, nums: List[int], k: int) -> bool:
+        count = 0
+        begin = True
+        
+        for number in nums:
+            if number == 1:
+                if begin == True:
+                    begin = False
+                elif count >= k:
+                    count = 0
+                else:
+                    return False
+            else:
+                count += 1
+                       
+        return True
